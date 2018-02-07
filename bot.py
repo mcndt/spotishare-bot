0b42b1f38d33fd12350000eca4bd1adff27732a4
mcndt <maximiocndt@gmail.com>
2018-02-07 21:33:07 +0100

    added role assignment when on_ready

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import json
 import discord
 
+
 # loading settings
 token, role_name = None, None
 with open('settings.json') as file:
@@ -19,6 +20,16 @@ async def on_ready():
     print('ID       : %s' % client.user.id)
     print('role name: \'%s\'' % role_name)
     print('-------')
+
+    # assign correct groups upon initialization
+    for server in client.servers:
+        listen_role = discord.utils.get(server.roles, name=role_name)
+
+        for member in server.members:
+            if str(member.game) == 'Spotify':
+                await client.add_roles(member, listen_role)
+            else:
+                await client.remove_roles(member, listen_role)
 
 
 # Group switching on listening
