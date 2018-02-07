9519a34bbb2a7f8d6b67fe5c1dd30cd1d3aee0c4
mcndt <maximiocndt@gmail.com>
2018-02-07 21:09:40 +0100

    changes role on game change to 'Spotify'

---
+++ bot.py
@@ -0,0 +1,41 @@
+import json
+import discord
+
+# loading settings
+token, role_name = None, None
+with open('settings.json') as file:
+    d = json.load(file)
+    token = d['token']
+    role_name = d['role_name']
+
+client = discord.Client()
+
+
+# startup sequence
+@client.event
+async def on_ready():
+    print('--------')
+    print('Bot name : %s' % client.user.name)
+    print('ID       : %s' % client.user.id)
+    print('role name: \'%s\'' % role_name)
+    print('-------')
+
+
+# Group switching on listening
+@client.event
+async def on_member_update(before, after):
+    game0, game1 = str(before.game), str(after.game)
+
+    if game0 != game1:
+        listen_role = discord.utils.get(after.server.roles, name=role_name)
+
+        if str(after.game) == 'Spotify':
+            await client.add_roles(after, listen_role)
+        else:
+            await client.remove_roles(after, listen_role)
+
+        print('[%s]: {%s}: %s -> %s' % (after.server.name,
+                                        after.name, game0, game1))
+
+
+client.run(token)
