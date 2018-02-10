36d69df8eefeaa7a65c3b50d3a17a99e8db01c97
mcndt <maximiocndt@gmail.com>
2018-02-10 13:41:23 +0100

    code cleanup

--- bot.py
+++ bot.py
@@ -3,7 +3,6 @@ import discord
 
 
 # loading settings
-token, role_name = None, None
 with open('settings.json') as file:
     d = json.load(file)
     token = d['token']
@@ -19,8 +18,8 @@ async def on_ready():
     print('Bot name : %s' % client.user.name)
     print('ID       : %s' % client.user.id)
     print('role name: \'%s\'' % role_name)
+    print('Active servers: %d' % len(client.servers))
     print('-------')
-
     # assign correct groups upon initialization
     for server in client.servers:
         listen_role = discord.utils.get(server.roles, name=role_name)
@@ -32,21 +31,18 @@ async def on_ready():
                 await client.remove_roles(member, listen_role)
 
 
-# Group switching on listening
+# Group switching when member starts listening
 @client.event
 async def on_member_update(before, after):
-    game0, game1 = str(before.game), str(after.game)
-
-    if game0 != game1:
+     if str(before.game) != str(after.game):
         listen_role = discord.utils.get(after.server.roles, name=role_name)
 
         if str(after.game) == 'Spotify':
             await client.add_roles(after, listen_role)
+            print('[%s]: %s is now listening' % (after.server.name, after.name))
         else:
             await client.remove_roles(after, listen_role)
-
-        print('[%s]: {%s}: %s -> %s' % (after.server.name,
-                                        after.name, game0, game1))
+            print('[%s]: %s stopped listening' % (after.server.name, after.name))
 
 
 client.run(token)
